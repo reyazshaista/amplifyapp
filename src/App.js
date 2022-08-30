23e64e6874b49ad98118fb0cb7edebd27e6fcb08
EC2 Default User <ec2-user@ip-10-1-0-37.ec2.internal>
2022-08-30 08:08:20 +0000

    mee

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        
+        <h1>Vendor App</h1>
       </header>
     </div>
   );
